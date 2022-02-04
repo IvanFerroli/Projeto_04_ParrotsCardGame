7c7d1b81275268963a76718a0012ccf645a35fdc
IvanFerroli <88122854+IvanFerroli@users.noreply.github.com>
2022-02-04 16:41:17 -0300

    Prompt com condições ok

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let cardsQuantity = prompt("Com quantas cartas você deseja jogar? Por favor, informe um número par entre 2 e 14")
+
+
+
+
+function howManyCards() {
+    let cardsQuantity = prompt("Com quantas cartas você deseja jogar? Por favor, informe um número par entre 2 e 14")
+    if(cardsQuantity % 2 == 0 && cardsQuantity >= 2 && cardsQuantity <= 14) {
+        renderCards();
+    }else{
+        howManyCards();
+    }
+
+    function renderCards() {
+        const container = document.querySelector(".container")
+        container.innerHTML
+    }
+}
+howManyCards();
+
+
