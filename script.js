4f81f70de0a3d2a33e765752d6db36f5746c5860
IvanFerroli <88122854+IvanFerroli@users.noreply.github.com>
2022-02-06 04:36:55 -0300

    checagem de pares e flip-onclick ok

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
+function startGame() {
 
-
-
-
-function howManyCards() {
-    let cardsQuantity = prompt("Com quantas cartas você deseja jogar? Por favor, informe um número par entre 2 e 14")
+    const  cardsQuantity = prompt("Com quantas cartas você deseja jogar? Por favor, informe um número par entre 2 e 14")
+    const cards = document.querySelectorAll('.card');
+    
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
+    
     if(cardsQuantity % 2 == 0 && cardsQuantity >= 2 && cardsQuantity <= 14) {
-        renderCards();
+        alert('Tudo certo, Vamos começar!');
     }else{
-        howManyCards();
+        startGame();
     }
-
-    function renderCards() {
-        const container = document.querySelector(".container")
-        container.innerHTML
+  
+    
+    function flipCard() {
+      if (lockBoard) return;
+      if (this === firstCard) return;
+    
+      this.classList.add('flip');
+    
+      if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    
+        return;
+      }
+    
+      secondCard = this;
+      checkForMatch();
     }
-}
-howManyCards();
-
-
+    
+    function checkForMatch() {
+      let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+      isMatch ? disableCards() : unflipCards();
+    }
+    
+    function disableCards() {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+    
+      resetBoard();
+    }
+    
+    function unflipCards() {
+      lockBoard = true;
+    
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    
+        resetBoard();
+      }, 1500);
+    }
+    
+    function resetBoard() {
+      [hasFlippedCard, lockBoard] = [false, false];
+      [firstCard, secondCard] = [null, null];
+    }
+    
+    (function shuffle() {
+      cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+      });
+    })();
+    
+    cards.forEach(card => card.addEventListener('click', flipCard));
+    
+    
+  }
+  startGame();
+  
